28d4afe0527dd6a14a5993cd7763ac7d31100a57
Hiago Chagas <hiago.chagas@premiersoft.net>
2023-09-27 18:50:45 -0300

    Initial Commit

---
+++ TimetableScheduler/TimetableSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimetableSchedulerApp.swift
+//  TimetableScheduler
+//
+//  Created by PremierSoft on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TimetableSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
